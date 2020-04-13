4732da210fc85209a8f63a8f8eae0f3e1a83ae18
Jack Lansing <jack.lansing@live.com>
2020-04-13 17:23:55 -0400

    updated migrations for posts feature

---
+++ migrations/003.undo.create_reconnect_posts.sql
@@ -0,0 +1,6 @@
+BEGIN;
+DROP TABLE IF EXISTS reconnect_posts;
+DROP TYPE IF EXISTS device_type;
+DROP TYPE IF EXISTS device_location;
+DROP TYPE IF EXISTS device_condition;
+COMMIT;
--- seeds/seed.reconnect_tables.sql
+++ seeds/seed.reconnect_tables.sql
@@ -1,11 +1,13 @@
 BEGIN;
 
-TRUNCATE reconnect_users RESTART IDENTITY CASCADE;
+TRUNCATE reconnect_users, reconnect_posts RESTART IDENTITY CASCADE;
 
 INSERT INTO reconnect_users (user_name, password, display_name, user_type)
 VALUES
 ('picard', 'ncc1701', 'Jean-Luc', 'Donor'),
 ('klingonw', 'password', 'Worf', 'Seeking'),
 ('troi123', 'farpoint87', 'Deanna', 'Seeking');
+
+
 
 COMMIT;
