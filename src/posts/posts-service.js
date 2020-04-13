26ef4e81fcbc94e7cd4311a39917fb14eeec0743
Jack Lansing <jack.lansing@live.com>
2020-04-13 17:38:25 -0400

    setup basic helper service for posts router

---
+++ src/posts/posts-service.js
@@ -0,0 +1,30 @@
+const xss = require(xss);
+const PostsService = {
+  getAllPosts(db) {
+    return db('reconnect_posts').select('*');
+  },
+  getById(db, id) {
+    return db('reconnect_posts').where({ id }).first();
+  },
+  insertPost(db, newPost) {
+    return db('reconnect_posts')
+      .insert(newPost)
+      .returning('*')
+      .then(([post]) => post)
+      .then(post => PostsService.getById(db, post.id));
+  },
+  serializePost(post) {
+    return {
+      id: post.id,
+      title: xss(post.title),
+      description: xss(post.description),
+      device: post.device,
+      condition: post.condition,
+      location: post.location,
+      date_created: post.date_created,
+      user_id: post.user_id
+    };
+  }
+};
+
+module.exports = PostsService;
