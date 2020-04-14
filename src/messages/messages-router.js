52607f0945c95d2b1ff756bb71d28653c81281c4
Jack Lansing <jack.lansing@live.com>
2020-04-14 12:57:04 -0400

    setup service and routes for new message thread and message

--- src/messages/messages-router.js
+++ src/messages/messages-router.js
@@ -5,39 +5,45 @@ const { requireAuth } = require('../middleware/jwt-auth');
 const messagesRouter = express.Router();
 const bodyParser = express.json();
 
-messagesRouter.route('/').post(bodyParser, async (req, res, next) => {
-  const { content, thread_id, author_id } = req.body;
-  const newMessage = { content, thread_id, author_id };
+messagesRouter
+  .route('/')
+  .all(requireAuth)
+  .post(bodyParser, async (req, res, next) => {
+    const { content, thread_id } = req.body;
+    const newMessage = { content, thread_id };
 
-  for (const [key, value] of Object.entries(newMessage)) {
-    if (value == null) {
-      return res.status(400).json({
-        error: `Missing '${key}' in request body`
-      });
+    for (const [key, value] of Object.entries(newMessage)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`
+        });
+      }
     }
-  }
 
-  try {
-    const message = await MessagesService.postNewMessage(
-      req.app.get('db'),
-      newMessage
-    );
+    const author_id = req.user.id;
 
-    res
-      .status(201)
-      .location(path.posix.join(req.originalUrl, `/${message.id}`))
-      .json(MessagesService.serializeMessage(message));
-  } catch (e) {
-    next(e);
-  }
-});
+    newMessage.author_id = author_id;
+
+    try {
+      const message = await MessagesService.postNewMessage(
+        req.app.get('db'),
+        newMessage
+      );
+
+      res
+        .status(201)
+        .location(path.posix.join(req.originalUrl, `/${message.id}`))
+        .json(MessagesService.serializeMessage(message));
+    } catch (e) {
+      next(e);
+    }
+  });
 
 messagesRouter
   .route('/new-thread')
   .all(requireAuth)
   .post(bodyParser, async (req, res, next) => {
     const { recipient_id } = req.body;
-
     if (!recipient_id) {
       return res.status(400).json({
         error: `Missing recipient_id in request body`
@@ -57,7 +63,7 @@ messagesRouter
         participants
       );
 
-      res.status(201).json({ thread });
+      res.status(201).json(thread);
     } catch (e) {
       next(e);
     }
