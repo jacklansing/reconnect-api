e0f6714f5004ac068ce25f3e80de0d386ba8ebbf
Jack Lansing <jack.lansing@live.com>
2020-04-14 12:16:46 -0400

    messages route set up

--- src/messages/messages-router.js
+++ src/messages/messages-router.js
@@ -1,14 +1,66 @@
 const express = require('express');
+const path = require('path');
+const MessagesService = require('./messages-service');
 const { requireAuth } = require('../middleware/jwt-auth');
 const messagesRouter = express.Router();
 const bodyParser = express.json();
 
 messagesRouter.route('/').post(bodyParser, async (req, res, next) => {
-  res.send('ok');
+  const { content, thread_id, author_id } = req.body;
+  const newMessage = { content, thread_id, author_id };
+
+  for (const [key, value] of Object.entries(newMessage)) {
+    if (value == null) {
+      return res.status(400).json({
+        error: `Missing '${key}' in request body`
+      });
+    }
+  }
+
+  try {
+    const message = await MessagesService.postNewMessage(
+      req.app.get('db'),
+      newMessage
+    );
+
+    res
+      .status(201)
+      .location(path.posix.join(req.originalUrl, `/${message.id}`))
+      .json(MessagesService.serializeMessage(message));
+  } catch (e) {
+    next(e);
+  }
 });
 
 messagesRouter
   .route('/new-thread')
-  .post(bodyParser, async (req, res, next) => {});
+  .all(requireAuth)
+  .post(bodyParser, async (req, res, next) => {
+    const { recipient_id } = req.body;
+
+    if (!recipient_id) {
+      return res.status(400).json({
+        error: `Missing recipient_id in request body`
+      });
+    }
+
+    const author_id = req.user.id;
+
+    const participants = {
+      recipient_id,
+      author_id
+    };
+
+    try {
+      const thread = await MessagesService.postNewThead(
+        req.app.get('db'),
+        participants
+      );
+
+      res.status(201).json({ thread });
+    } catch (e) {
+      next(e);
+    }
+  });
 
 module.exports = messagesRouter;
