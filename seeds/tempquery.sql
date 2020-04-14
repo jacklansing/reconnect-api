798d5817e215404361c4f0f84120b8106cd58c93
Jack Lansing <jack.lansing@live.com>
2020-04-14 18:28:03 -0400

    removed scratch file was using to write queries, whoops!

--- seeds/tempquery.sql
+++
@@ -1,9 +0,0 @@
-SELECT DISTINCT ON (m.thread_id) 
-m.thread_id, m.content, m.author_id, m.date_created
-FROM reconnect_messages m INNER JOIN reconnect_messages_threads t  
-ON m.thread_id = t.id ORDER BY m.thread_id, m.date_created DESC
-
-SELECT DISTINCT ON (m.thread_id) 
-m.thread_id, m.content, m.author_id, m.date_created
-FROM reconnect_messages m INNER JOIN reconnect_messages_threads t  
-ON m.thread_id = t.id WHERE t.recipient_id = 4 OR t.author_id = 4 ORDER BY m.thread_id, m.date_created DESC
