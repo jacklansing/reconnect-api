75727aba57cdba6594bd8aeac54f77b6029c221d
Jack Lansing <jack.lansing@live.com>
2020-04-14 11:46:14 -0400

    setup messages service object

--- src/messages/messages-router.js
+++ src/messages/messages-router.js
@@ -3,8 +3,12 @@ const { requireAuth } = require('../middleware/jwt-auth');
 const messagesRouter = express.Router();
 const bodyParser = express.json();
 
-messagesRouter.route('/').post(async (req, res, next) => {
+messagesRouter.route('/').post(bodyParser, async (req, res, next) => {
   res.send('ok');
 });
+
+messagesRouter
+  .route('/new-thread')
+  .post(bodyParser, async (req, res, next) => {});
 
 module.exports = messagesRouter;
--- src/messages/messages-service.js
+++ src/messages/messages-service.js
@@ -1,3 +1,35 @@
-const MessagesService = {};
+const xss = require('xss');
+
+const MessagesService = {
+  postNewThead(db, participants) {
+    return db('reconnect_messages_threads')
+      .insert(participants)
+      .returning('*')
+      .then(([thread]) => thread)
+      .then(thread => MessagesService.getThreadById(db, thread.id));
+  },
+  getThreadById(db, id) {
+    return db('reconnect_messages_threads').where({ id }).first();
+  },
+  postNewMessage(db, message) {
+    return db('reconnect_messages')
+      .insert(message)
+      .returning('*')
+      .then(([message]) => message)
+      .then(message => MessagesService.getMessageById(db, message.id));
+  },
+  getMessageById(db, id) {
+    return db('reconnect_messages').where({ id }).first();
+  },
+  serializeMessage(message) {
+    return {
+      id: message.id,
+      content: xss(message.content),
+      thread_id: message.thread_id,
+      author_id: message.author_id,
+      date_created: message.date_created
+    };
+  }
+};
 
 module.exports = MessagesService;
