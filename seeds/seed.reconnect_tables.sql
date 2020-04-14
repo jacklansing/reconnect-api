80d7fb7b204837f11a15c871e5edc8f066e55e6e
Jack Lansing <jack.lansing@live.com>
2020-04-14 11:28:03 -0400

    updated db seed file

--- seeds/seed.reconnect_tables.sql
+++ seeds/seed.reconnect_tables.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-TRUNCATE reconnect_users, reconnect_posts RESTART IDENTITY CASCADE;
+TRUNCATE reconnect_users, reconnect_posts, reconnect_messages, reconnect_messages_threads RESTART IDENTITY CASCADE;
 
 INSERT INTO reconnect_users (user_name, password, display_name, user_type)
 VALUES
@@ -13,5 +13,16 @@ VALUES
 ('Old iPhone 6', 'Well taken care of, in quite good condition.', 'iPhone', 'good', 'Albany, NY', 1),
 ('Pixel 2 - Good Shape!', 'The pixel 2 is still a great phone to get connected with. I took very good care of mine.', 'Android', 'very good', 'Albany, NY', 2),
 ('Thinkpad', 'Used to use this as a work computer before upgrading. Slightly beat up but still holds a charge!', 'Windows', 'okay', 'Schenectady, NY', 3);
+
+INSERT INTO reconnect_messages_threads (author_id, recipient_id)
+VALUES
+(1, 2),
+(3, 2);
+
+INSERT INTO reconnect_messages (content, thread_id, author_id)
+VALUES
+('Hey there im interested in your old smartphone. It belongs in a museum!', 1, 1),
+('Hey would you be able to meet me on Mars?', 2, 3);
+
 
 COMMIT;
